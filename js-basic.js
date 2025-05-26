6b3c70bc0ce5affafe053b7957f5941eee4868f7
Andrisboostit <arday.andras@boostit.hu>
2025-05-26 19:02:06 +0200

    Házi frissítve.

--- js-basic.js
+++ js-basic.js
@@ -116,3 +116,7 @@ return osszeg / szamok.length
 }
 
 console.log(`A tömb átlaga: ${atlag(szamok)}`); //kiíratom az átlagot
+
+let email = "valami@valami.hu"
+let regex =/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+console.log(`Az email cím helyes: ${regex.test(email)}`); //itt létrehozok egy reguláris kifejezést, ami ellenőrzi az email cím formátumát, majd kiíratom a teszt eredményét
