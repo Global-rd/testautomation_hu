7561e0e5d20c2cb6d4f13756dc331f5fceb5146f
gyorgyice85 <gyorgyice85@gmail.com>
2025-05-29 14:34:13 +0200

    Update js-basics.js

--- js-basics.js
+++ js-basics.js
@@ -34,8 +34,9 @@
     let space= /\s/.test(fullName);
     console.log(space); // true
 
-4.)  //Tömb
-     let numbers = [3, 5, 8, 2, 10];  
+4.)  
+    //Tömb
+    let numbers = [3, 5, 8, 2, 10];  
     for (i = 0; i < numbers.length; i++) {
         console.log(numbers[i]);          //A tömb elemeinek kiíratása
     }
@@ -45,7 +46,8 @@
             console.log(numbers[i]);      //A tömb páros elemeinek kiíratása
 
 
-5.) //Objektum létrehozása egy User reprezentálására
+5.) 
+    //Objektum létrehozása egy User reprezentálására
     let user = {
         name: "Anna Kiss",
         age: 25,
@@ -55,3 +57,28 @@
     function outputUser(user) {
         console.log("A(z) " + user.name + " nevü felhasználó " + user.age + " éves és az email címe: " + user.email + "."); 
     }
+
+6.)
+    //Az atlagSzamitas függvény bemenetként kap egy tömböt és visszaadja annak az átlagát.
+    function atlagSzamitas(numbers) {
+      let summe = 0;
+      let atlag = 0;
+      for (let i = 0; i < numbers.length; i++) {
+          summe = summe + numbers[i];
+          atlag = summe / numbers.length;
+        
+      }
+      console.log("Az átlag: " + atlag);
+    }
+          
+    atlagSzamitas([1,2,3]); //Az átlag: 2
+
+
+7.)
+    //Email cím ellenőrzése regex-szel
+    function emailEllenorzes(email) {
+    let regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    let valasz = regex.test(email);
+    console.log(valasz); // true, ha érvényes az email cím, false, ha nem
+    }
+          
