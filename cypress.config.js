044db02ca53aa1a81ef2760e1057baa4963c4b8c
Ádám L. Juhász <jadaml@gmail.com>
2025-06-22 10:29:44 +0200

    1. Cypress config testreszabása

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'https://automationteststore.com/',
+    defaultCommandTimeout: 8000
   },
 });
