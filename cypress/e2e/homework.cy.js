cbe66c05bb5c2e86c4727aa4509694a95361e952
Ádám L. Juhász <jadaml@gmail.com>
2025-06-24 13:39:05 +0200

    Added comment to 2–5 tests.

--- cypress/e2e/homework.cy.js
+++ cypress/e2e/homework.cy.js
@@ -3,11 +3,23 @@
 describe('4. Házi feladat spec', () => {
   it('2. Terméklista rendezés tesztje', () => {
     cy.visit('/index.php?rt=product/category&path=36'); // Makeup
-    cy.get('#sort').select('pd.name-ASC');
+    // Kijelölöm a rendezés opciót,
+    cy.get('#sort')
+      // és kiválasztom, hogy A-Z sorrendben rendezze.
+      // Az opciót az értéke alapján jelölöm ki.
+      .select('pd.name-ASC');
+    // Az oldal ekkor újra tölt.
+
     const products = [];
+    // Kijelölöm az összes termék nevet.
+    // A fejlesztő elgépelte az osztály nevét,
+    // és a jobb kezével fáziskésésben volt.
     cy.get('.thumbnails.grid a.prdocutname') // SIC
+      // Minden elemnek egy tömbbe gyűjtöm csak a szövegét,
       .each(element => products.push(element.text().trim()))
       .then(() => {
+        // majd a tömböt összehasonlítom az ABC sorrenbe rendezett változatával.
+        // A feladat csak az első 3 elemre kérdezett, így a két tömböt 3 elemre szorítom meg.
         expect(products.slice(0, 3)).to.have.ordered.members(products.sort().slice(0, 3));
       })
   });
@@ -17,14 +29,18 @@ describe('4. Házi feladat spec', () => {
     const prices = [];
     const products = [];
 
+    // Hogy a termék biztosan raktáron legyen, a kezdő oldalon lévő legújabb termékekből választok ki 2 terméket.
     cy.get('#latest').within(() => {
+      // Kiválasztom az összes terméket reprezentáló elemeket.
       const thumbnails = cy.get('.thumbnails > div');
 
+      // Kosárba teszem az elsőt, és feljegyzem a nevét és az árát.
       thumbnails.first().within(() => {
         cy.addToCart();
         cy.getPrice().then(price => prices.push(price));
         cy.getProductName().then(product => products.push(product));
       })
+      // Ugyanezt teszem a másodikkal.
       .next().within(() => {
         cy.addToCart();
         cy.getPrice().then(price => prices.push(price));
@@ -32,16 +48,26 @@ describe('4. Házi feladat spec', () => {
       });
     });
 
+    // Betöltjük a kosár tartalmát.
     cy.visit('/index.php?rt=checkout/cart')
+    // Sajnos a táblázatnak egyik része sincs rendesen beazonosíthatóan megjelölve,
+    // így az elemek sorszám szerinti kiválasztásával kell kijelölnöm a vizsgált elemeket.
+    // A termékek nevét, árát és darabszámát, sorrendben akartam vizsgálni,
+    // így a konkrét elemet kellett kijelölnöm.
+
+    // Termék neve megegyezik
     cy.get('.cart-info table:first tr td:nth-child(2)').each((element, idx) => {
       expect(element.text().trim()).to.be.equal(products[idx]);
-    })
+    });
+    // Termék ára megegyezik
     cy.get('.cart-info table:first tr td:nth-child(4)').each((element, idx) => {
       expect(Number(element.text().substring(1))).to.be.equal(prices[idx]);
-    })
+    });
+    // Minden termékből csak 1 van a kosárban.
     cy.get('input[id^="cart_quantity"]').each((element, idx) => {
       expect(Number(element.val())).to.be.equal(1);
-    })
+    });
+    // A termékek összege helyesen van kiválasztva.
     cy.get('#totals_table > tbody > :nth-child(1) > :nth-child(2)').then(element => {
       expect(Number(element.text().substring(1)))
         .to.equal(prices.reduce((acc, val) => acc += val));
@@ -50,6 +76,9 @@ describe('4. Házi feladat spec', () => {
 
   it('4. Termék kategóriák', () => {
     cy.visit('/');
+    // A (fő) kategóriák listájára nincs külön oldal,
+    // ezért a főoldalon, a navigációs listában jelöljük ki.
+    // Ezen listában ugyanúgy szerepel a home gomb is, amit nem akarunk beleszámolni.
     cy.get('#categorymenu .nav-pills > li > a:not(a.menu_home)')
       .should('have.length', 7);
   });
@@ -57,17 +86,25 @@ describe('4. Házi feladat spec', () => {
   it ('5. Regisztrációs űrlap hibakezelése', () => {
     cy.visit('/index.php?rt=account/create');
 
+    // Az oldal meglátogatásakor a form üres,
+    // így azt üresen küldjük el.
     cy.get('#AccountFrm').submit();
 
+    // Megvizsgáljuk, hogy megjelenik-e egy összesítő hiba üzenet,
     cy.get('.alert')
       .should('be.visible')
-      .contains('agree to the Privacy Policy').should('exist');
+      // amiben csak a Privacy Policy elfogadására szóló üzenet szerepel.
+      .contains('agree to the Privacy Policy')
+      .should('exist');
 
+    // Elfogadjuk a Privacy Policy-t, és újra küldjük a formot.
     cy.get('#AccountFrm_agree').check();
     cy.get('#AccountFrm').submit();
 
+    // Megvizsgáljuk, hogy megjelenik-e egy összesítő hiba üzenet,
     cy.get('.alert')
       .should('be.visible')
+      // És megnézzük, hogy az összes hibás mező benne szerepel.
       .contains('Login name')
       .contains('First Name')
       .contains('Last Name')
@@ -78,6 +115,9 @@ describe('4. Házi feladat spec', () => {
       .contains('region / state')
       .contains('Password')
       .should('exist');
+
+    // Először a vezeték nevet töltjük ki rosszul.
+    // A kitétel 1–32 karakter közé korlátozza a bevitelt.
 
     cy.get('#AccountFrm_firstname')
       .type('123456789012345678901234567890123');
@@ -96,6 +136,9 @@ describe('4. Házi feladat spec', () => {
     cy.get('.alert')
       .contains('First Name').should('not.exist');
 
+    // Következő: keresztnév
+    // 1–32
+
     cy.get('#AccountFrm_lastname')
       .type('123456789012345678901234567890123');
     cy.get('#AccountFrm').submit();
@@ -113,6 +156,9 @@ describe('4. Házi feladat spec', () => {
     cy.get('.alert')
       .contains('Last Name').should('not.exist');
 
+    // Következő: email
+    // Formátuma helyes legyen
+
     cy.get('#AccountFrm_email')
       .type('user@example.com');
     cy.get('#AccountFrm').submit();
@@ -121,8 +167,20 @@ describe('4. Házi feladat spec', () => {
       .contains('Email')
       .should('not.exist');
 
+    // Következő: 1. címsor
+    // 3–128
+
     cy.get('#AccountFrm_address_1')
-      .type('123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789');
+      .type('12');
+    cy.get('#AccountFrm').submit();
+
+    cy.get('.alert')
+      .should('be.visible')
+      .contains('Address 1')
+      .should('exist');
+
+    cy.get('#AccountFrm_address_1')
+      .type('3456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789');
     cy.get('#AccountFrm').submit();
 
     cy.get('.alert')
@@ -137,7 +195,10 @@ describe('4. Házi feladat spec', () => {
 
     cy.get('.alert')
       .contains('Address 1').should('not.exist');
-    
+
+    // Következő: város
+    // 3–128
+
     cy.get('#AccountFrm_city')
       .type('12');
     cy.get('#AccountFrm').submit();
@@ -164,6 +225,9 @@ describe('4. Házi feladat spec', () => {
     cy.get('.alert')
       .contains('City').should('not.exist');
 
+    // Következő: Régió
+    // Legyen kiválasztva
+
     cy.get('#AccountFrm_zone_id')
       .select(1);
     cy.get('#AccountFrm').submit();
@@ -171,6 +235,9 @@ describe('4. Házi feladat spec', () => {
     cy.get('.alert')
       .contains('region / state')
       .should('not.exist');
+
+    // Következő: Irányítószám
+    // 3–10
 
     cy.get('#AccountFrm_postcode')
       .type('12');
@@ -185,6 +252,8 @@ describe('4. Házi feladat spec', () => {
       .type('345678901');
     cy.get('#AccountFrm').submit();
 
+    // A teszt ezen a ponton elbukik,
+    // mert bár a hiba jelzésben nem mehet tovább 10-nél, de az oldal elfogadja.
     cy.get('.alert')
       .should('be.visible')
       .contains('Zip/postal code')
