4dcc2599301740545c5204edb3610faeb280b4cd
nagytamy <nagy.tamy@gmail.com>
2025-08-21 23:50:03 +0200

    Update baseurl

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
     e2e: {
-        baseUrl: 'http://localhost:8080/parabank',
+        baseUrl: 'https://parabank.parasoft.com/parabank',
         defaultCommandTimeout: 8000,
         reporter: 'json',
         reporterOptions: {
