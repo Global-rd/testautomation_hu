08f9cab80f7713cfe764461cb0da4173110dffe2
gyorgyi <gyorgyice85@gmail.com>
2025-08-31 01:22:39 +0200

    8. hazi feladat

---
+++ support/helpers.js
@@ -0,0 +1,17 @@
+// Constants
+export const CALCULATOR_URL = 'https://www.online-calculator.com//html5/simple/index.php?v=10';
+export const CANVAS_RENDER_DELAY = 200;
+export const TAB_OPEN_DELAY = 1000;
+
+// Helper function to get canvas center position
+export const getCanvasCenter = async (page) => {
+    const boundingBox = await page.evaluate(() => {
+        const canvas = document.querySelector('canvas');
+        return canvas?.getBoundingClientRect();
+    });
+
+    return {
+        x: boundingBox.x + (Number(boundingBox?.width) / 2),
+        y: boundingBox.y + (Number(boundingBox?.height) / 2)
+    };
+}; 
